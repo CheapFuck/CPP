42de2c68089d89fd0f5d8f912762f73e49dd1238
Thijs Van Den Berg <thivan-d@f1r5s18.codam.nl>
2025-05-12 13:04:51 +0200

    05

---
+++ 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,16 @@
+// constant name
+// grade 1-150
+// Bureaucrat::GradeTooHighException
+// Bureaucrat::GradeTooLowException.
+// getName()
+// getGrade()
+//incrementGrade()
+//decrementGrade()
+#pragma once
+#include <cstring>
+#include <iostream>
+
+class Bureaucrat
+{
+
+};
